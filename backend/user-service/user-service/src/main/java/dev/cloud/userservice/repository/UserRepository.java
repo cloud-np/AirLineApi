0728dd5d9d4f7b7f00b21f2e29d83f5a44110a89
cloud-np <nikos.pappas@solera.com>
2022-11-30 10:23:24 +0200

    Added the correct user-service

--- backend/user-service/user-service/src/main/java/dev/cloud/userservice/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.cloud.userservice.repository;
-
-import dev.cloud.userservice.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
